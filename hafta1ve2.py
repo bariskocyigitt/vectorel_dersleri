3a8d9895ecf1bd221c46a6674a85d6d3df6758b4
bariskocyigit <barisk2000@gmail.com>
2025-05-04 17:03:42 +0300

    duzenleme

---
+++ hafta1ve2.py
@@ -0,0 +1,33 @@
+
+
+print("vEKTOREL BİLİSİM","Python98 grubu","print dersi",sep="--",end="***")
+
+"""
+dewdwdew
+dedwdew
+dwedwe
+"""
+
+'''
+dedeedd
+dede
+'''
+sinav1=100
+sinav2=95
+
+"""
+print(f"{sinav1} ve {sinav2} için [ortalaman: {ortalama}")
+print("{} ve {} için ortalama: {}".format(sinav1,sinav2,ortalama))
+"""
+
+
+
+xxx="Vektorel Biisim"
+print(xxx)
+print(xxx[2])
+print(xxx[2:])
+print(xxx[2:7])
+print(xxx[2:15:3])
+print(xxx[:5])
+print(xxx[-3:])
+
